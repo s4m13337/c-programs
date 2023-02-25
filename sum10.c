d0c97ad5104c1d4ba59f8e05afbb1c5a3b3b362b
s4m13337 <ee.sam@protonmail.com>
2023-02-25 21:30:42 +0100

    Program to compute sum of first 10 natural numbers

---
+++ sum10.c
@@ -0,0 +1,11 @@
+/* C program to compute the sum of first 10 natural numbers */
+
+#include<stdio.h>
+
+int main(){
+	int i, result=0;
+	for(i=1; i<=10; i++){
+		result += i;
+	}
+	printf("The sum of first 10 numbers is %d.\n", result);
+}
